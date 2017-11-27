079c7625c94e0d76aaca8739762ff599dfbfec93
ked <kedthunder@gmail.com>
2017-11-27 11:57:26 +0100

    Added comments

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,13 @@
 $(function() {
 	
 	/*Variables globales */
+	/*Nom: globals
+	Utilité: Contient les infos du joueur dans les variables suivantes
+			-name : Le nom du joueur
+			-unitNumber : Le nombre d'unités qu'il possède (ici des madamada)
+			-generatorNumber : Le nombre de générateurs
+			-boughGeneratorNumber : le nombre de générateurs qu'il a acheté (pour incrémenter le prix)
+			-generatorceptionNumber : le nombre de générateurceptions qu'il a acheté*/
 	var globals = {
 		name : "",
 		unitNumber : 0,
@@ -8,15 +15,25 @@ $(function() {
 		boughGeneratorNumber : 0, // Pour différencier le nombre de générateurs achetés et possédés 
 		generatorceptionNumber : 0
 	};
-	
+	/* Nom: identified
+	 * Utilité: pour savoir si l'utilisateur à envoyé son nom (et donc afficher la bonne partie de la page) 
+	 */
 	var identified = false;
 	
-
+	/* 	Mise à jour des valeurs sur la page toutes les secondes
+	 */
 	setInterval(function() {
 		update();
 	}, 1000);
 
 	/* fonctions */
+	
+	/* 	Fonctionnalité : mise à jour
+	 *	Nom de la fonction : update
+	 *	Description : appelle les différentes fonctions de mise à jour
+	 *	Paramètres : aucun
+	 *	Sorties : aucune
+	 */
 	function update() {
 		updateUnits();
 		updateGenerators();
@@ -25,16 +42,35 @@ $(function() {
 		updateReqGeneratorceptions();
 	}
 
+	/* 	Fonctionnalité : ajout d'unités
+	 *	Nom de la fonction : addUnit
+	 *	Description : incrémente le nombre d'unités et sauvegarde sur le serveur
+	 *	Paramètres : aucun
+	 *	Sorties : aucune
+	 */
 	function addUnit() {
 		++globals.unitNumber;
 		save();
 	}
-
+	
+	/* 	Fonctionnalité : mise à jour du texte (compteur) sur la page
+	 *	Nom de la fonction : update units
+	 *	Description : Ajoute le nombre de générateurs et met à jour le texte du compteur sur la page
+	 *	Paramètres : aucun
+	 *	Sorties : aucune
+	 */
 	function updateUnits() {
 		globals.unitNumber += globals.generatorNumber;
 		$(" #nbMadamada ").text(globals.unitNumber);
 	}
-
+	
+	/* 	Fonctionnalité : ajoute un générateur
+	 *	Nom de la fonction : addGenerator
+	 *	Description : Ajoute un générateur si il y a assez d'unités et déduit le nombre d'unités si
+	 *					un générateur est acheté
+	 *	Paramètres : aucun
+	 *	Sorties : aucun
+	 */
 	function addGenerator() {
 		var reqUnitNumber = 30 + globals.boughGeneratorNumber * 10;
 		if (globals.unitNumber >= reqUnitNumber) {
@@ -43,16 +79,32 @@ $(function() {
 			globals.unitNumber -= reqUnitNumber;
 		}
 	}
-
+	/* 	Fonctionnalité : Mise à jour du nombre d'unités requises pour acheter un générateur
+	 *	Nom de la fonction : updateReqGenerators
+	 *	Description : Met à jour le nombre d'unités requises pour acheter un générateur
+	 *	Paramètres : aucun
+	 *	Sorties : aucun
+	 */
 	function updateReqGenerators() {
 		$(" #reqNbGen ").text(30 + globals.boughGeneratorNumber * 10);
 	}
-
+	
+	/* 	Fonctionnalité : Mise à jour du nombre de générateur
+	 *	Nom de la fonction : updateGenerators
+	 *	Description : Incrémente et met à jour le nombre de générateurs
+	 *	Paramètres : aucun
+	 *	Sorties : aucune
+	 */
 	function updateGenerators() {
 		globals.generatorNumber += globals.generatorceptionNumber;
 		$(" #nbGen ").text(globals.generatorNumber);
 	}
-
+	/* 	Fonctionnalité : ajoute un générateurception
+	 *	Nom de la fonction : addGeneratorception
+	 *	Description : ajouter un générateurception si il y a assez d'unités et déduit le nombre requis
+	 *	Paramètres : aucun
+	 *	Sorties : aucune
+	 */
 	function addGeneratorception() {
 		var reqUnitNumber = 92 + globals.generatorceptionNumber * 1128;
 		if (globals.unitNumber >= reqUnitNumber) {
@@ -60,36 +112,54 @@ $(function() {
 			globals.unitNumber -= reqUnitNumber;
 		}
 	}
-
+	/* 	Fonctionnalité : mise à jour du nombre d'unités requises pour un générateurception
+	 *	Nom de la fonction : updateReqGeneratorceptions
+	 *	Description : met à jour le nombre d'unités requises pour acheter un générateurception
+	 *	Paramètres : aucun
+	 *	Sorties : aucunes
+	 */
 	function updateReqGeneratorceptions() {
 		$(" #reqNbGenception ").text(92 + globals.generatorceptionNumber * 1128);
 	}
-
+	/* 	Fonctionnalité : mise à jour du nombre de générateursceptions possédés
+	 *	Nom de la fonction : updateGeneratorceptions
+	 *	Description : met à jour le nombre de générateursceptions possédés
+	 *	Paramètres : aucun
+	 *	Sorties : aucun
+	 */
 	function updateGeneratorceptions() {
 		$(" #nbGenception ").text(globals.generatorceptionNumber);
 	}
 	
+	//Cache la partie de jeu par défaut (l'utilisateur doit donner son pseudo avant)
 	$("#identified").hide();
+	//Quand l'utilisateur click sur le bouton "jouer"
 	$("#identButton").click(function(){
 		play($("#name").val());
 	});
-
+	//Le handler du click sur l'image de madamada
 	$(" #madamada ").click(function() {
 		addUnit();
 	})
-
+	//Le handler du click sur l'image du générateur
 	$(" #generateur ").click(function() {
 		addGenerator();
 	})
-
+	//Le handler du clikc sur l'image du générateurception
 	$(" #generateurception ").click(function() {
 		addGeneratorception();
 	})
-
+	//L'interval de sauvegarde 
 	setInterval(function() {
 		save();
 	}, 5000);//On sauvegarde toutes les 5 secondes
-
+	
+	/* 	Fonctionnalité : Envoi des données au serveur
+	 *	Nom de la fonction : save
+	 *	Description : on met dans un String l'objet globals, on le log et on l'envoie au serveur
+	 *	Paramètres : aucun
+	 *	Sorties : aucune
+	 */
 	function save() {
 		if (identified) {
 			var json = JSON.stringify(globals);
@@ -98,6 +168,14 @@ $(function() {
 		}
 	}
 	
+	/* 	Fonctionnalité : Initialisation du jeu pour le client
+	 *	Nom de la fonction : play
+	 *	Description : envoie le nom du joueur au serveur et récupère un objet JSON sous forme
+	 *					de String. Transpose ensuite les données de cet objet dans la variable globale
+	 *					globals
+	 *	Paramètres : val(Le nom du joueur)
+	 *	Sorties : 
+	 */
 	function play(val){
 		if(val != null){
 			globals.name = val;
--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -2,6 +2,15 @@ var url = require('url');
 
 var querystring = require("querystring"), fs = require("fs");
 
+/* Variable globale
+Nom: savedObj
+Utilité: Contient un tableau d'objets, chaque objet contient les variables suivantes
+		-name : Le nom du joueur
+		-unitNumber : Le nombre d'unités qu'il possède (ici des madamada)
+		-generatorNumber : Le nombre de générateurs
+		-boughGeneratorNumber : le nombre de générateurs qu'il a acheté (pour incrémenter le prix)
+		-generatorceptionNumber : le nombre de générateurceptions qu'il a acheté
+*/
 var savedObj = {
 	table : []
 }
@@ -17,9 +26,20 @@ setInterval(function() {
 	})
 }, 30000);
 
+/* 	Fonctionnalité : Sauvegarde
+ *	Nom de la fonction : save
+ *	Description : Vérifie que l'initialisation à été faite, sinon on la fait
+ *					On va ensuite créer un objet globals avec les variables reçues
+ *					On regarde ensuite si l'objet est déjà dans le tableau
+ *					Si oui on l'enlève
+ *					Et on rajoute le nouveau
+ *	Paramètres : response, request
+ *	Sorties : aucune (envoi d'une réponse vide au client)
+ */
 function save(response, request) {
 	console.log("Le gestionnaire de sauvegarde est appelé.");
-
+	
+	//Vérifie que l'initialisation à été faite, sinon on la fait
 	if (savedObj.table[0] == null && !readTextFile('./save.json')) {
 		init();
 	}
@@ -28,30 +48,27 @@ function save(response, request) {
 	var qdata = q.query;
 
 	
-
+		//On va ensuite créer un objet globals avec les variables reçues
 		var globals = {
 			name : qdata.name,
 			unitNumber : qdata.unitNumber,
 			generatorNumber : qdata.generatorNumber,
-			boughGeneratorNumber : qdata.boughGeneratorNumber, // Pour
-																// différencier
-			// le nombre de
-			// générateurs
-			// achetés et
-			// possédés
+			boughGeneratorNumber : qdata.boughGeneratorNumber, 
 			generatorceptionNumber : qdata.generatorceptionNumber
 		};
-
+		
+		//On regarde ensuite si l'objet est déjà dans le tableau
 		savedObj.table.forEach(function(item, index, arr) {
 			if (item.name === globals.name) {
-				savedObj.table.splice(index, 1);
+				savedObj.table.splice(index, 1);//Si oui on l'enlève
 			}
 		});
-
+		
+		//Et on rajoute le nouveau
 		savedObj.table.push(globals);
 
-		var JSONString = JSON.stringify(savedObj);
-		console.log("JSon string: " + JSONString);
+		/*var JSONString = JSON.stringify(savedObj);
+		console.log("JSon string: " + JSONString);*/
 	
 	response.setHeader("Access-Control-Allow-Origin", "*");
 	response.writeHead(200, {
@@ -59,15 +76,21 @@ function save(response, request) {
 	});
 	response.end();
 }
-
+/* 	Fonctionnalité : Chargement
+ *	Nom de la fonction : load
+ *	Description : Envoie les données au client en fonction de son nom
+ *	Paramètres : response, request
+ *	Sorties : reponse au client (ses données)
+ */
 function load(response, request) {
-
+	//Si l'initialisation n'a pas été faite, on la fait
 	if (savedObj.table[0] == null && !readTextFile('./save.json')) {
 		init();
 	}
 
 	var q = url.parse(request.url, true);
 	var qdata = q.query;
+	//On initialise l'objet globals avec toutes les variables nécéssaires
 	var globals = {
 		name : "",
 		unitNumber : 0,
@@ -77,12 +100,13 @@ function load(response, request) {
 	};
 
 	console.log("Getting player by name where name is :" + qdata.name);
+	//Pour chaque objet sauvegardé on compare les noms 
 	savedObj.table.forEach(function(item, index, arr) {
 		if (item.name != null) {
 			console.log(item);
 			var compare = item.name.localeCompare(qdata.name);
 			console.log(compare);
-			if (compare == 0) {
+			if (compare == 0) {//Si c'est le même on rempli globals
 				console.log(item);
 				globals.name = item.name;
 				globals.unitNumber = item.unitNumber;
@@ -94,12 +118,13 @@ function load(response, request) {
 	});
 	console.log("Objet renvoyé");
 	console.log(globals);
+	//On converti en String de JSON l'objet globals
 	var resp = JSON.stringify(globals);
 	response.setHeader("Access-Control-Allow-Origin", "*");
 	response.writeHead(200, {
 		"Content-Type" : "text/html"
 	});
-	if (resp != null && resp.name != "") {
+	if (resp != null && resp.name != "") {//Si l'objet de retour n'est pas null on renvoie les données
 		console.log("Sent respond");
 		response.write(resp);
 	} else
@@ -108,7 +133,12 @@ function load(response, request) {
 	response.end();
 
 }
-
+/* 	Fonctionnalité : Initialisation
+ *	Nom de la fonction : init
+ *	Description : Lecture du fichier JSON de sauvegarde
+ *	Paramètres : aucun
+ *	Sorties : aucun
+ */
 function init() {
 	console.log("Chargement du fichier de sauvegarde JSON");
 	// Get content from file
@@ -119,7 +149,12 @@ function init() {
 	savedObj = save;
 	
 }
-
+/* 	Fonctionnalité : Teste si un objet est vide
+ *	Nom de la fonction : isEmpty
+ *	Description : Teste si un objet est vide
+ *	Paramètres : aucun
+ *	Sorties : boolean
+ */
 function isEmpty(obj) {
 	for ( var prop in obj) {
 		if (obj.hasOwnProperty(prop))
@@ -128,13 +163,18 @@ function isEmpty(obj) {
 
 	return true;
 }
-
+/* 	Fonctionnalité : lecture d'un fichier
+ *	Nom de la fonction : readTextFile
+ *	Description : lit un fichier text 
+ *	Paramètres : filePath(le chemin du fichier)
+ *	Sorties : boolean
+ */
 function readTextFile(filePath) {
 	try {
 		var save = fs.readFileSync(filePath);
 	} catch (e) {
 		console.log("Le fichier de sauvegarde n'éxiste pas");
-		return true;
+		return false;
 	}
 
 	return isEmpty(save);
